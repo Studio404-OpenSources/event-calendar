5fff058ecc0ebe4e4c68b70b18f0e49e8e17d550
Giorgi <opensource@404.ge>
2016-06-14 18:17:13 +0400

    add form

--- calendar.php
+++ calendar.php
@@ -1,34 +1,16 @@
 <?php
 class calendar{
     
-    private $currentYear = 0;
-    private $currentMonth = 0;
-    private $currentDay = 0;
-    private $currentDate = null;
-    private $daysInMonth = 0;
-
-    private function requests($type,$item){
-		if($type=="POST" && isset($_POST[$item])){
-			return filter_input(INPUT_POST, $item);
-		}else if($type=="GET" && isset($_GET[$item])){
-			return filter_input(INPUT_GET, $item);
-		}else{
-			return '';
-		}
-	}
-    
     public function show($opt) {
     	$this->option = $opt;
     	$this->dayLabels = $this->option['dayLabels'];
     	$this->monthLabel = $this->option['monthLabel'];
-    	$this->naviHref = $this->option['slug'];    	
-                
-        $month = null;
+    	$this->naviHref = $this->option['slug'];  
          
         if($this->requests('GET','year')){
  			$year = $this->requests('GET','year');
         }else{
-            $year = date("Y", time());  
+            $year = date("Y", time()); 
         }         
          
         if($this->requests('GET','month')){
@@ -41,46 +23,69 @@ class calendar{
          
         $this->currentYear = $year;
         $this->currentMonth = $month;
-        $this->daysInMonth = $this->_daysInMonth($month, $year);  
+        $this->currentDay = 0;
+        $this->daysInMonth = $this->daysInMonth($month, $year);  
          
  		$content = sprintf(
- 			'<table style="%s">
+ 			'<table style="%s" cellpadding="0" cellspacing="1">
  			<tr style="%s">
  			<td colspan="7">%s</td>
  			</tr>
+ 			<tr>%s</tr>
  			',
  			$this->arrayToStyle($this->option['css']['calendar']), 
  			$this->arrayToStyle($this->option['css']['header']), 
- 			$this->_createNavi()
+ 			$this->createNavi(),
+ 			$this->createLabels()
  		);
 
-
- 		$content .= sprintf(
- 			'<tr>%s</tr>',
- 			$this->_createLabels()
- 		);
-
- 		$weeksInMonth = $this->_weeksInMonth($month,$year);
-
- 		$f = 1;
+ 		$weeksInMonth = $this->weeksInMonth($month,$year);
  		for( $i=0; $i<$weeksInMonth; $i++ ){
  			$content .= '<tr>';
 			for($j=1;$j<=7;$j++){
-				$content .= $this->_showDay($i*7+$j);
+				$content .= $this->showDay($i*7+$j);
 			}
 			$content .= '</tr>';
 		}
-
-
- 		$content .= sprintf(
- 			'</table>'
- 		);
-		
+		if($this->option['addEvents']){
+			$content .= sprintf(
+			'<tr>
+			<td colspan="7">
+			<form action="%s" method="POST" style="%s">
+			<label style="%s">თარიღი:</label>
+			<input type="text" name="calendar_date" value="%s" style="%s" />
+			<label style="%s">დასახელება:</label>
+			<input type="text" name="calendar_title" value="" style="%s" />
+			<input type="submit" name="calendar_submit" value="შენახვა" style="%s" />
+			</form>
+			</td>
+			</tr>',
+			$this->option['slug'],
+			$this->arrayToStyle($this->option['css']['form']),
+			$this->arrayToStyle($this->option['css']['label']),
+			date("d-m-Y"), 
+			$this->arrayToStyle($this->option['css']['input_text']), 
+			$this->arrayToStyle($this->option['css']['label']),
+			$this->arrayToStyle($this->option['css']['input_text']), 
+			$this->arrayToStyle($this->option['css']['input_submit']) 
+			);
+		}
+ 		$content .= '</table>';		
 
         return $content;   
     }
 
-    private function _showDay($cellNumber){
+    private function requests($type,$item){
+		if($type=="POST" && isset($_POST[$item])){
+			return filter_input(INPUT_POST, $item);
+		}else if($type=="GET" && isset($_GET[$item])){
+			return filter_input(INPUT_GET, $item);
+		}else{
+			return '';
+		}
+	}
+
+    private function showDay($cellNumber){
         if($this->currentDay==0){
             $firstDayOfTheWeek = date('N',strtotime($this->currentYear.'-'.$this->currentMonth.'-01'));
 			if(intval($cellNumber) == intval($firstDayOfTheWeek)){
@@ -107,7 +112,7 @@ class calendar{
 		return $out;
     }
 
-    private function _createNavi(){
+    private function createNavi(){
 		$nextMonth = $this->currentMonth==12?1:intval($this->currentMonth)+1;
 		$nextYear = $this->currentMonth==12?intval($this->currentYear)+1:$this->currentYear;
 		$preMonth = $this->currentMonth==1?12:intval($this->currentMonth)-1;
@@ -120,9 +125,13 @@ class calendar{
         );
         
         $out = sprintf(
-        	'<a style="%s" href="%s?month=%02d&year=%s">უკან</a>
-        	<div style="%s">%s</div>
-        	<a style="%s" href="%s?month=%02d&year=%s">წინ</a>',
+        	'<table width="100&#37;" cellspacing="0" cellpadding="0">
+        	<tr>
+        	<td width="25&#37;"><a style="%s" href="%s?month=%02d&year=%s">უკან</a></td>
+        	<td width="50&#37;"><div style="%s">%s</div></td>
+        	<td width="25&#37;"><a style="%s" href="%s?month=%02d&year=%s">წინ</a></td>
+        	</tr>
+        	</table>',
         	$this->arrayToStyle($this->option['css']['prev']), 
         	$this->naviHref, 
         	$preMonth, 
@@ -139,7 +148,7 @@ class calendar{
     }
 
 
-    private function _createLabels(){  
+    private function createLabels(){  
         $content = '';
         foreach($this->dayLabels as $index => $label){
         	$content .= sprintf(
@@ -151,7 +160,7 @@ class calendar{
 		return $content;
     }
      
-    private function _weeksInMonth($month=null,$year=null){
+    private function weeksInMonth($month=null,$year=null){
         if(null==($year)){
             $year =  date("Y",time()); 
         }
@@ -160,7 +169,7 @@ class calendar{
 			$month = date("m",time());
         }
 
-        $daysInMonths = $this->_daysInMonth($month,$year);
+        $daysInMonths = $this->daysInMonth($month,$year);
         $numOfweeks = ($daysInMonths%7==0?0:1) + intval($daysInMonths/7);
         $monthEndingDay= date('N',strtotime($year.'-'.$month.'-'.$daysInMonths));
         $monthStartDay = date('N',strtotime($year.'-'.$month.'-01'));
@@ -173,7 +182,7 @@ class calendar{
     }
  
 
-    private function _daysInMonth($month=null,$year=null){
+    private function daysInMonth($month=null,$year=null){
         if(null==($year))
             $year =  date("Y",time()); 
  		if(null==($month))
--- index.php
+++ index.php
@@ -19,49 +19,66 @@ require 'calendar.php';
 }
 </style>
 </head>
-<body style="margin:0px; padding:0px">
+<body>
 <?php
 $container_width = '100%';
 $opt = array(
-	"dayLabels" => array("ორშ","სამ","ოთხ","ხუთ","პარ","შაბ","კვი"), 
-	"monthLabel" => array("იან","თებ","მარ","აპრ","მაი","ივნ","ივლ","აგვ","სექ","ოქტ","ნოე","დეკ"), 
+	"addEvents" => true,  
+	"dayLabels" => array(
+		"ორშ",
+		"სამ",
+		"ოთხ",
+		"ხუთ",
+		"პარ",
+		"შაბ",
+		"კვი"
+	), 
+	"monthLabel" => array(
+		"იანვარი",
+		"თებერვალი",
+		"მარტი",
+		"აპრილი",
+		"მაისი",
+		"ივნისი",
+		"ივლისი",
+		"აგვისტო",
+		"სექტემბერი",
+		"ოქტომბერი",
+		"ნოემბერი",
+		"დეკემბერი"
+	), 
 	"slug"=>"index.php",
 	"css"=>array(
 		"calendar"=>array(
 			"margin"=>"0px auto",
 			"padding"=>"0px", 
 			"width"=>$container_width, 
-			"font-family"=>"serif"
+			"font-family"=>"serif", 
+			"border-top"=>"solid 4px #3c8dbc"
 		), 
 		"header"=>array(
-			"line-height"=>"40px", 
+			"line-height"=>"60px", 
 			"position"=>"relative",
 			"height"=>"40px", 
 			"text-align"=>"center",
-			"background-color"=>"#787878"
 		),
 		"prev"=>array(
 			"cursor"=>"pointer",
 			"text-decoration"=>"none",
-			"color"=>"#FFF",
 			"float"=>"left",
-			"margin-left"=>"10px"
+			"margin-left"=>"10px", 
+			"color"=>"#787878"
 		),
 		"title"=>array(
 			"margin"=>"0px",
 			"padding"=>"0px",
-			"color"=>"#FFF",
-			"font-size"=>"18px",
-			"width"=>"150px",
+			"color"=>"#787878",
 			"text-align"=>"center",
-			"position"=>"absolute",
-			"left"=>"calc(50% - 75px)",
-			"top"=>"0px",
 		),
 		"next"=>array(
 			"cursor"=>"pointer",
 			"text-decoration"=>"none",
-			"color"=>"#FFF",
+			"color"=>"#787878",
 			"float"=>"right",
 			"margin-right"=>"10px"
 		),
@@ -69,17 +86,18 @@ $opt = array(
 			"height"=>"40px", 
 			"line-height"=>"40px", 
 			"text-align"=>"center", 
-			"color"=>"#000", 
-			"font-size"=>"15px", 
-			"background-color"=>"#f2f2f2"
+			"color"=>"#ffffff", 
+			"font-size"=>"14px", 
+			"background-color"=>"#3c8dbc"
 		),
 		"days"=>array(
 			"height"=>"80px", 
 			"font-size"=>"25px", 
-			"background-color"=>"#DDD",
+			"background-color"=>"#ffffff",
 			"color"=>"#000",
 			"text-align"=>"center", 
-			"position"=>"relative"
+			"position"=>"relative",
+			"border"=>"solid 1px #dddddd"
 		),
 		"days_number"=>array(
 			"margin"=>"0px", 
@@ -91,6 +109,32 @@ $opt = array(
 			"position"=>"absolute", 
 			"bottom"=>"10px", 
 			"right"=>"10px"
+		),
+		"form"=>array(
+			"margin"=>"10px 0", 
+			"padding"=>"0px"
+		),
+		"label"=>array(
+			"margin"=>"0px", 
+			"padding"=>"10px 0px",
+			"width"=>"100%",
+			"display"=>"block", 
+			"color"=>"#787878"
+		),
+		"input_text"=>array(
+			"margin"=>"0px", 
+			"padding"=>"0 5px",
+			"width"=>"100%",
+			"height"=>"30px", 
+			"color"=>"#787878"
+		),
+		"input_submit"=>array(
+			"margin"=>"10px 0 0 0", 
+			"padding"=>"10px 20px",
+			"border"=>"0px",
+			"cursor"=>"pointer",
+			"background-color"=>"#3c8dbc",
+			"color"=>"#ffffff"
 		),
 		"clear"=>array(
 			"clear"=>"both"
